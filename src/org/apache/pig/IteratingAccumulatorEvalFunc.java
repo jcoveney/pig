64d4a38081e7b7edcf31fd76a1eff0b683cbc928
Jonathan Coveney <jcoveney@twitter.com>
2012-05-10 17:04:41 -0700

    Add tests for IteratingAccumulatorEvalFunc

--- src/org/apache/pig/IteratingAccumulatorEvalFunc.java
+++ src/org/apache/pig/IteratingAccumulatorEvalFunc.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.pig;
 
 import java.io.IOException;
@@ -10,6 +27,9 @@ import org.apache.pig.data.DataBag;
 import org.apache.pig.data.Tuple;
 import org.apache.pig.data.TupleFactory;
 
+/**
+ *
+ */
 public abstract class IteratingAccumulatorEvalFunc<T> extends AccumulatorEvalFunc<T> implements TerminatingAccumulator<T> {
     private boolean isInitialized = false;
     private DelayedQueueIterator dqi;
@@ -102,7 +122,7 @@ public abstract class IteratingAccumulatorEvalFunc<T> extends AccumulatorEvalFun
         isInitialized = false;
     }
 
-    public abstract T exec(Iterator<Tuple> t) throws IOException;
+    public abstract T exec(Iterator<Tuple> iter) throws IOException;
 
     private class DelayedQueueIterator implements Iterator<Tuple> {
         private Tuple next;
