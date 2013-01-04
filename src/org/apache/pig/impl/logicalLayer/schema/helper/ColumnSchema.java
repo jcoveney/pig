faaea205b9f38f153a32fda1955ba3fc649af65d
Jonathan Coveney <jcoveney@gmail.com>
2013-01-04 12:52:02 -0800

    work

--- src/org/apache/pig/impl/logicalLayer/schema/helper/ColumnSchema.java
+++ src/org/apache/pig/impl/logicalLayer/schema/helper/ColumnSchema.java
@@ -1,5 +1,6 @@
 package org.apache.pig.impl.logicalLayer.schema.helper;
 
+import org.apache.pig.data.DataType;
 import org.apache.pig.impl.logicalLayer.FrontendException;
 import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 
@@ -25,4 +26,40 @@ public abstract class ColumnSchema {
 	}
 
 	public abstract FieldSchema toFieldSchema(boolean fillInNullAliases) throws FrontendException;
+
+	public boolean isBag() {
+	  return DataType.BAG == getDataType();
+	}
+
+	public boolean isTuple() {
+	  return DataType.TUPLE == getDataType();
+	}
+
+	public boolean isMap() {
+	  return DataType.MAP == getDataType();
+	}
+
+	public boolean isBoolean() {
+	  return DataType.BOOLEAN == getDataType();
+	}
+
+	public boolean isInteger() {
+	  return DataType.INTEGER == getDataType();
+	}
+
+	public boolean isLong() {
+	  return DataType.LONG == getDataType();
+	}
+
+	public boolean isFloat() {
+	  return DataType.FLOAT == getDataType();
+	}
+
+	public boolean isDouble() {
+	  return DataType.DOUBLE == getDataType();
+	}
+
+	public boolean isChararray() {
+	  return DataType.CHARARRAY == getDataType();
+	}
 }
