9cdbdf15c6dfd7c20727f9becd21a47e4b3ef585
Jonathan Coveney <jcoveney@gmail.com>
2013-01-15 16:10:22 -0800

    insanity

---
+++ src/org/apache/pig/parser/WittgensteinLogicalPlanBuilder.java
@@ -0,0 +1,9 @@
+package org.apache.pig.parser;
+
+/**
+ * This is the bridge between Wittgenstein (in Scala) and the Pig parser
+ */
+public class WittgensteinLogicalPlanBuilder {
+    public static class WLogicalOperator
+
+}
